ad7552b379bfeaee89b02bea5aa77cd684a8af44
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2023-10-12 23:19:57 +0100

    Added spdlog as submodule

--- premake5.lua
+++ premake5.lua
@@ -3,5 +3,9 @@ workspace "concurrent-server-client"
     architecture "x86_64"
     startproject "client"
 
+    filter { "configurations:Debug or configurations:Release" }
+        includedirs { "thirdparty/spdlog/include" }
+        defines { "SC_LOGGING" }
+
 include "src/server.lua"
 include "src/client.lua"
