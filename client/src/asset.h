73013011d4c99229b9cb0193774161bdad068f71
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2023-10-23 23:57:08 +0100

    Created asset manager singleton class

---
+++ client/src/asset.h
@@ -0,0 +1,39 @@
+#pragma once
+
+template <typename T>
+class AssetManager;
+
+class Asset
+{
+public:
+    Asset()
+        : m_asset_id{}
+    {
+    }
+
+    Asset(const Asset&) = default;
+    Asset& operator=(const Asset&) = default;
+
+    Asset(Asset&&) noexcept = default;
+    Asset& operator=(Asset&&) noexcept = default;
+
+    virtual ~Asset() = default;
+
+    virtual void Load() = 0;
+    virtual void Unload() = 0;
+
+    [[nodiscard]] size_t GetAssetId() const
+    {
+        return m_asset_id;
+    }
+
+protected:
+    size_t m_asset_id;
+
+    void SetAssetId(const size_t id)
+    {
+        m_asset_id = id;
+    }
+
+    friend AssetManager;
+};
---
+++ client/src/asset_manager.cpp
@@ -0,0 +1,5 @@
+#include "asset_manager.h"
+
+#include "rendering/shader.h"
+
+AssetManager<Shader> AssetManager<Shader>::s_instance{};
