c5965fce343aa954905f657562c4b6ced0f20d40
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2023-12-21 14:41:58 +0000

    Added documentation to ThreadPool

--- server/src/thread_pool.h
+++ server/src/thread_pool.h
@@ -13,17 +13,42 @@ enum class ThreadState
     Working
 };
 
+/**
+ * \brief A data structure implemented as a singleton pattern to manage a collection
+ * of reusable threads.
+ */
 class ThreadPool
 {
 public:
     ThreadPool(ThreadPool&&) noexcept = delete;
     ThreadPool& operator=(ThreadPool&&) noexcept = delete;
 
+    /**
+     * \brief Initialises each of the threads to be managed by the thread pool.
+     * It specifies the initial state and active status of each created thread.
+     */
     static void Initialise();
+
+    /**
+     * \brief Gets the active status of the thread with the given identifier.
+     * \param id The identifier of the thread.
+     * \return A true or false value indicating whether the thread is active.
+     */
     static bool GetThreadActiveStatus(UUID id);
+
+    /**
+     * \brief Gets the state of the thread with the given identifier.
+     * \param id The identifier of the thread.
+     * \return The current state of the thread.
+     */
     static ThreadState GetThreadState(UUID id);
 
 private:
+    /**
+     * \brief Represents a thread as an object with a state and activity status.
+     * Typically, C++ threads do not have associated members, but this allows us
+     * to provide them.
+     */
     struct Thread
     {
         std::thread handle;
