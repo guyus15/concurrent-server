4c45092580829fde26ccc4a3f6111086796379c6
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2024-02-16 16:18:23 +0000

    Added levels to the client asset manager

--- client/src/asset_manager.cpp
+++ client/src/asset_manager.cpp
@@ -3,8 +3,13 @@
 
 #include <common/assets/asset_manager.h>
 
+#include <common/level.h>
+
 template <>
 AssetManager<Shader> AssetManager<Shader>::s_instance{};
 
 template <>
 AssetManager<Texture2d> AssetManager<Texture2d>::s_instance{};
+
+template <>
+AssetManager<Level> AssetManager<Level>::s_instance{};
